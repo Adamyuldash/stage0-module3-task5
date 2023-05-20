d02951a2786190253b8a8f0849e96a50bcea5f6d
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-05-20 11:03:53 -0700

    f

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class TimeConvertor {
+
     public void convert(float minutes) {
+        Scanner minut = new Scanner(System.in);
+        System.out.println("Minutni kirgiz");
+        int num = minut.nextInt();
+        int s = num * 60;
+        System.out.println(s + " Sekund!");
     }
 }
